d2b65f80dd043b3cc847fb5cfdad905252d58517
RahulRajak525 <rahulrajak525@gmail.com>
2022-12-08 20:42:27 +0530

    TextField

---
+++ src/components/3MuiTextField.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Stack, TextField, InputAdornment } from "@mui/material";
+import { useState } from "react";
+
+const MuiTextField = () => {
+  const [value, setValue] = useState("");
+  return (
+    <Stack spacing={4}>
+      <Stack direction="row" spacing={2}>
+        <TextField label="Name" variant="outlined" />
+        <TextField label="Name" variant="filled" />
+        <TextField label="Name" variant="standard" />
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <TextField label="Small secondary" size="small" color="secondary" />
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <TextField
+          label="Form Input"
+          required
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          error={!value}
+          helperText={!value ? "Required" : "Do not share your password with anyone"
+          }
+        />
+        <TextField
+          label="Form Input"
+          helperText="Do not share your password with anyone"
+        />
+        <TextField
+          label="password"
+          type="password"
+          helperText="Do not share your password with anyone"
+          disabled
+        />
+        <TextField label="Read only" InputProps={{ readOnly: true }} />
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <TextField
+          label="Amount"
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="Weight"
+          InputProps={{
+            endAdornment: <InputAdornment position="start">Kg</InputAdornment>,
+          }}
+        />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiTextField;
