01fb9ee61fc111acce03dbffc1d64223516a3406
RahulRajak525 <rahulrajak525@gmail.com>
2022-12-08 23:57:01 +0530

    CheckBox

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import { MuiButton } from './components/2MuiButton';
 import { MuiTypography } from './components/1MuiTypography';
 import MuiTextField from './components/3MuiTextField';
 import MuiSelect from './components/4MuiSelect';
+import MuiRadioButton from './components/5MuiRadioButton';
+import MuiCheckBox from './components/6MuiCheckBox';
 
 function App() {
+  
   return (
     <div className="App">
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
       {/* <MuiTextField/> */}
-      <MuiSelect/>
+      {/* <MuiSelect/> */}
+      {/* <MuiRadioButton/> */}
+      <MuiCheckBox/>
 
     </div>
   );
