9d1dffc36d95093511295b6c4c6c1bb69edaedbe
RahulRajak525 <rahulrajak525@gmail.com>
2022-12-08 20:44:09 +0530

    All about Select

---
+++ src/components/4MuiSelect.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+import { Box, TextField, MenuItem } from "@mui/material"
+
+const MuiSelect = () => {
+  const [countries, setCountries] = useState<string[]>([]);
+  console.log({countries});
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setCountries(typeof value ==='string'? value.split(','): value);
+  };
+  return (
+    <Box width="250px">
+      <TextField
+        label="Select Country"
+        select
+        value={countries}
+        onChange={handleChange}
+        fullWidth
+        SelectProps={{
+          multiple: true,
+        }}
+        size="small"
+        color="secondary"
+        helperText='Please Select Your Country'
+        error
+      >
+        <MenuItem value="IN">India</MenuItem>
+        <MenuItem value="US">USA</MenuItem>
+        <MenuItem value="AU">Australia</MenuItem>
+      </TextField>
+    </Box>
+  );
+};
+
+export default MuiSelect;
